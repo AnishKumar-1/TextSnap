63fd151a24fa475919d945bef72e108ee792b36f
anish <kumaranish0815@gmail.com>
2025-03-10 16:08:09 +0530

    modified main class

--- src/main/java/com/TextSnap/TextSnapApplication.java
+++ src/main/java/com/TextSnap/TextSnapApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TextSnapApplication {
 
 	public static void main(String[] args) {
+		 String port = System.getenv("PORT"); // Get port from environment
+		    if (port == null || port.isEmpty()) {
+		        port = "8080"; // Default to 8080 if not set
+		    }
+		    System.setProperty("server.port", port); // Set the server port dynamically
 		SpringApplication.run(TextSnapApplication.class, args);
 	}
 
